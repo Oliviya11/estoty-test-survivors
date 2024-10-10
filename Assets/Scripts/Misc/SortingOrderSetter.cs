b3905130bf84bffe4763c502b58bdb61b8ef5180
Anastasiia Mitinova <nastyawnastya@gmail.com>
2024-10-10 14:35:00 +0300

    Add SortingOrderSetter

---
+++ Assets/Scripts/Misc/SortingOrderSetter.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Misc
+{
+    public class SortingOrderSetter : MonoBehaviour
+    {
+        [SerializeField] SpriteRenderer spriteRenderer;
+        [SerializeField] SpriteRenderer underSpriteRenderer;
+
+        void Update()
+        {
+            // Set the sorting order based on the y-coordinate of the object
+            int order = -(int)(transform.position.y * 10);
+            spriteRenderer.sortingOrder = order;
+            underSpriteRenderer.sortingOrder = order - 1;
+        }
+    }
+}
